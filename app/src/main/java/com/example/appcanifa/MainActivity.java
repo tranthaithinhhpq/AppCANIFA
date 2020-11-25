4eb31311b5c0f24e61a583b3a08b10fe34508fc1
Ngo Quang Nhat Duy <68750351+Farthen-Dur@users.noreply.github.com>
2020-11-25 17:49:00 +0700

    Clip3+4

--- app/src/main/java/com/example/appcanifa/MainActivity.java
+++ app/src/main/java/com/example/appcanifa/MainActivity.java
@@ -2,17 +2,31 @@ package com.example.appcanifa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button joinNowButton, loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        joinNowButton =(Button) findViewById(R.id.main_join_now_btn);
+        loginButton = (Button) findViewById(R.id.main_login_btn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
