4c84ceeaeffaab3b7658ae0e5fdb003f37372157
tranthaithinhhpq <55739883+tranthaithinhhpq@users.noreply.github.com>
2020-11-27 22:22:59 +0700

    11 + 12 + 13

--- app/src/main/java/com/example/appcanifa/LoginActivity.java
+++ app/src/main/java/com/example/appcanifa/LoginActivity.java
@@ -118,7 +118,7 @@ public class LoginActivity extends AppCompatActivity {
                                 Toast.makeText(LoginActivity.this, "Welcome Admin,logged in successfully...", Toast.LENGTH_SHORT).show();
                                 loadingBar.dismiss();
 
-                                Intent intent = new Intent(LoginActivity.this, AdminAddNewProductActivity.class);
+                                Intent intent = new Intent(LoginActivity.this, AdminCategoryActivity.class);
                                 startActivity(intent);
 
                             }else if (parentDbName.equals("Users")){
