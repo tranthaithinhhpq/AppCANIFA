4c81320c22805706e67c0670e5cdf56789188e08
thinh202 <55739822+thinh202@users.noreply.github.com>
2020-11-29 18:49:01 +0700

    Buoi16

--- app/src/main/java/com/example/appcanifa/HomeActivity.java
+++ app/src/main/java/com/example/appcanifa/HomeActivity.java
@@ -1,15 +1,25 @@
 package com.example.appcanifa;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.appcanifa.Model.Products;
 import com.example.appcanifa.Prevalent.Prevalent;
+import com.example.appcanifa.ViewHolder.ProductViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+import androidx.annotation.NonNull;
+import androidx.collection.CircularArray;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -17,18 +27,24 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class HomeActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+    private DatabaseReference ProductsRef;
+    private RecyclerView recyclerView;
+     RecyclerView.LayoutManager layoutManager;
+    private CircularArray Picasso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
+        ProductsRef = FirebaseDatabase.getInstance().getReference().child("Products");
 
         Toolbar toolbar = findViewById(R.id.toolbar);
 
@@ -51,11 +67,66 @@ public class HomeActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-        View headerView =navigationView.getHeaderView(0);
-        TextView userNameTextView=headerView.findViewById(R.id.user_profile_name);
-        CircleImageView profileImageView =headerView.findViewById(R.id.user_profile_image);
+        View headerView = navigationView.getHeaderView(0);
+        TextView userNameTextView = headerView.findViewById(R.id.user_profile_name);
+        CircleImageView profileImageView = headerView.findViewById(R.id.user_profile_image);
         userNameTextView.setText(Prevalent.currentOnlineUser.getName());
+
+        recyclerView = findViewById(R.id.recycler_menu);
+         recyclerView.setHasFixedSize(true);
+            layoutManager = new LinearLayoutManager(this);
+             recyclerView.setLayoutManager(layoutManager);
     }
+
+
+    @Override
+    public void onStart() {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        super.onStart();
+        FirebaseRecyclerOptions<Products> options =
+                new FirebaseRecyclerOptions.Builder<Products>()
+                        .setQuery(ProductsRef, Products.class)
+                        .build();
+
+
+        FirebaseRecyclerAdapter<Products, ProductViewHolder> adapter =
+                new FirebaseRecyclerAdapter<Products, ProductViewHolder>(options) {
+                    @NonNull
+                    @Override
+                    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                        return null;
+                    }
+
+                    @Override
+                    protected void onBindViewHolder(@NonNull ProductViewHolder holder, int i, @NonNull Products products) {
+                        
+                    }
+
+                    @Override
+                    protected void onBindViewHolder(@NonNull ProductViewHolder holder) {
+                        Products model;
+                        holder.txtProductName.setText(model.getPname());
+                        holder.txtProductDescription.setText(model.getDescription());
+                          holder.txtProductPrice.setText( "Price = "+ model.getPrice() + "$");
+                                Picasso.get().load(model.getImage()).into(holder.imageView);
+                    }
+                }
+
+
+        @NonNull
+        @Override
+        public ProductViewHolder onCreateViewHolder (@NonNull ViewGroup parent ,int viewType){
+
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_items_layout,parent,false);
+            ProductViewHolder holder = new ProductViewHolder(view);
+             return holder ;
+        }
+    };
+          recyclerView.setAdapter(adapter);
+            adapter.startListening();
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
